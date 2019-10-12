6d8c28af8b51462f7ee69a1f8b3d2784567fdaa2
kanedev <kanedev@live.fr>
2019-10-13 01:54:03 +0200

    🎉 init

---
+++ test1.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+# create blank window
+window = Tk()
+window.title("my first app in python")
+
+
+
+# PROBABLY THE MOST IMPORTANT PART!!!!!!!!!!
+
+# if you forget to call the mainloop function, nothing will appear to the user
+window.mainloop()
+
